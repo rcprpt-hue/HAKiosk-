60188723c4d7c88271af0ea8aa44a5946b44ba0b
rcprpt-hue <rcprpt@gmail.com>
2026-06-18 12:48:29 +0200

    Add files via upload

--- custom_components/hakiosk/config_flow.py
+++ custom_components/hakiosk/config_flow.py
@@ -9,13 +9,15 @@ class HAKioskConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            # Here we could validate connection to the tablet
+            # Clean host: remove http:// or https:// and trailing slashes
+            host = user_input[CONF_HOST].replace("http://", "").replace("https://", "").split("/")[0].split(":")[0]
+            user_input[CONF_HOST] = host
             return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_NAME, default="Tablet Sala"): str,
+                vol.Required(CONF_NAME, default="Tablet Entrada"): str,
                 vol.Required(CONF_HOST): str,
                 vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
                 vol.Required(CONF_DEVICE_ID): str,
